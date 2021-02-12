f7844c6b7aed8eb3780a748ac1ab7cf298eff979
钟坤林 <zkl@zkllab.com>
2021-02-12 20:54:40 +0800

    refactor: change redirect uri origin

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,8 +1,8 @@
 export const oidcSettings = {
   authority: 'https://sso.dev.cloud.shuosc.com/auth/realms/shuosc',
   clientId: 'bookshelf',
-  redirectUri: `http://localhost:8080/signin-callback.html`,
-  silentRedirectUri: 'http://localhost:8080/silent-signin-callback.html',
+  redirectUri: `${window.location.origin}/signin-callback.html`,
+  silentRedirectUri: `${window.location.origin}/silent-signin-callback.html`,
   automaticSilentRenew: true,
   responseType: 'code',
   scope: 'openid profile email',
